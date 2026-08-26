52ec1b1011c888ce902d3000eebbe54374cb7b63
塔奇巧克力 <nurockplayer@gmail.com>
2026-08-27 04:51:27 +0900

    chore: remove GitHub Pages artifact preparation

--- scripts/prepare-pages.ts
+++
@@ -1,8 +0,0 @@
-import { contentDistRoot } from './lib/books';
-import { preparePagesOutput, publishedReleaseSlugs } from './lib/pages';
-
-const bookSlugs = publishedReleaseSlugs(contentDistRoot());
-preparePagesOutput('dist', bookSlugs);
-console.log(
-  `ok   dist: GitHub Pages SPA fallback + ${bookSlugs.length} published Book route(s) prepared`,
-);
